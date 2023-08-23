5d11ab4e78f0398590bdcb03498b9082529ce933
oka-teruhiro <oka331@gmail.com>
2023-08-23 20:56:34 +0900

    5.0.4 2023.08.23 20:54 プロバイダ＆モデル形式でピンクの丸を動かす

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sanchu5b/main_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  //const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '天運三柱推命',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: '天運三柱推命ver.5.0.3'),
+      home: const HomePage(title: '天運三柱推命ver.5.0.4'),
     );
   }
 }
@@ -42,6 +44,31 @@ class HomePage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                Stack(
+                  children: [
+                    SizedBox(
+                      width: 312,
+                      height: 312,
+                      child: CustomPaint(
+                        painter: ShapePainter5(
+                          tuhen: model.tuhen,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  model.aText,
+                  style: const TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      // TODO ここで何かをする
+                      model.changeAText();
+                    },
+                    child: const Text('ボタン')),
                 const Text(
                   'You have pushed the button this many times:',
                 ),
@@ -63,5 +90,62 @@ class HomePage extends StatelessWidget {
         );
       }),
     );
+  }
+}
+
+class ShapePainter5 extends CustomPainter {
+  //int gogyou = 1; // 五行　0:木 1:火 2:土 3:金 4:水  // ■■■■■■受け取る値を代入する変数を定義
+  //int nitiSi = 0;
+  int tuhen; // 日し
+  ShapePainter5({required this.tuhen}); // ■■■■■■■■■受け取る値を上記変数に代入
+  @override
+  void paint(Canvas canvas, Size size) {
+    var center1 = const Offset(150, 130); // 表面の中心座標
+    var center2 = const Offset(150, 400); // 本質の中心座標
+
+    print('tuhen:$tuhen');
+
+    double rr1 = 18;
+    double r1 = 36;
+    double r2 = 72;
+    double rt;
+    double gogyo;
+    int gogyo1;
+
+    final penPink2 = Paint()
+      ..color = Colors.purple
+      ..strokeWidth = 2.0;
+    final penPink1 = Paint()
+      ..color = Colors.pink
+      ..strokeWidth = 2.0;
+
+    final penPink = Paint()
+      ..color = Colors.purpleAccent
+      ..strokeWidth = 2.0;
+    final penPink3 = Paint()
+      ..color = Colors.purple[300]!
+      ..strokeWidth = 2.0;
+
+    gogyo1 = tuhen % 2;
+    if (gogyo1 == 0) {
+      rt = r2;
+      gogyo = tuhen / 2;
+    } else {
+      rt = r1;
+      gogyo = (tuhen - 1) / 2;
+    }
+
+    double radianTu = (72 * gogyo - 90) / 180 * pi;
+    final centerTu = center1 + Offset(rt * cos(radianTu), rt * sin(radianTu));
+
+    canvas.drawCircle(centerTu, rr1 + 8, penPink2..style = PaintingStyle.fill);
+    canvas.drawCircle(centerTu, rr1 + 4, penPink1..style = PaintingStyle.fill);
+    canvas.drawCircle(centerTu, rr1 - 0, penPink..style = PaintingStyle.fill);
+    //canvas.drawCircle(centerTu, rr1 - 4, penPink3..style = PaintingStyle.fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -3,11 +3,25 @@ import 'package:flutter/material.dart';
 class MainModel extends ChangeNotifier {
   int c = 0;
   String ct = 'a';
+  String aText = 'こんにちは';
+  int tuhen = 0;
 
   void inc() {
     c++;
     ct = c.toString();
 
+    notifyListeners();
+  }
+
+  void changeAText() {
+    tuhen = (tuhen + 1) % 10;
+
+    if (aText == 'こんにちは') {
+      aText = 'こんばんは';
+    } else {
+      aText = 'こんにちは';
+    }
+    // aText = 'こんばんは';
     notifyListeners();
   }
 }
